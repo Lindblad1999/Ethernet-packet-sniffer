2121b95d09b274c67c1e61ff10c3bf9308af61ef
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2021-03-01 15:18:59 +0100

    new Parse

--- client.py
+++ client.py
@@ -8,6 +8,8 @@ s.bind(("eth0", 0))
 r=s.recvfrom(2000)
 # sys.stdout.write("<%s>\n"%repr(r))
 
+
+# packet string from tuple
 packet = r[0]
 
 # take first 20 characters for the ip header
@@ -22,21 +24,31 @@ ihl = version_ihl & 0xF
 
 iph_length = ihl * 4
 
-eth_length = 14
-eth_header = packet[:eth_length]
-eth = struct.unpack('!6s6sH', eth_header)
-eth_protocol = socket.ntohs(eth[2])
-t = iph_length + eth_length
-tcp_header = packet[t:t+20]
+ttl = iph[5]
+protocol = iph[6]
+s_addr = socket.inet_ntoa(iph[8]);
+d_addr = socket.inet_ntoa(iph[9]);
 
-tcph = struct.unpack('!HHLLBBHHH' , tcp_header)
+print('Version : ' + str(version) + ' IP Header Length : ' + str(ihl) + ' TTL : ' + str(ttl) + ' Protocol : ' + str(protocol) + ' Source Address : ' + str(s_addr) + ' Destination Address : ' + str(d_addr))
+
+tcp_header = packet[iph_length:iph_length + 20]
+
+# now unpack them :)
+tcph = struct.unpack('!HHLLBBHHH', tcp_header)
+
 source_port = tcph[0]
 dest_port = tcph[1]
 sequence = tcph[2]
 acknowledgement = tcph[3]
 doff_reserved = tcph[4]
 tcph_length = doff_reserved >> 4
-h_size = eth_length + iph_length + tcph_length * 4
+
+print('Source Port : ' + str(source_port) + ' Dest Port : ' + str(dest_port) + ' Sequence Number : ' + str(sequence) + ' Acknowledgement : ' + str(acknowledgement) + ' TCP header length : ' + str(tcph_length))
+
+h_size = iph_length + tcph_length * 4
 data_size = len(packet) - h_size
 
+# get data from the packet
 data = packet[h_size:]
+
+print('Data : ' + data)
