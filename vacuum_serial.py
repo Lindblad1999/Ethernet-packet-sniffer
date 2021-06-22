f6ba6dc6bf5528b360f3652c65d453d7c7806f70
Lindblad1999 <emil.lindblad.christensen@gmail.com>
2021-06-22 11:51:39 +0200

    formatting missing

--- vacuum_serial.py
+++ vacuum_serial.py
@@ -2,17 +2,28 @@
 
 import os, sys
 import serial
-from prometheus_client import Gauge, Summary, start_http_server
+from prometheus_client import Gauge, start_http_server
 import time
 
-REQUEST_TIME = Summary('request_processing_seconds', 'Time spent processing request')
-
+# Serial object
 ser = serial.Serial('/dev/ttyUSB0', 9600, timeout=5)
 
+# Format the data, to fit prometheus
+def format_data(data):
+    pass
+
+# Read the serial port, and set the gauge metric
+def process_request():
+    data = ser.readline()
+    data = data.decode('utf-8')
+    print(data)
+    data = format_data(data)
+    gauge.set(data)
+
 if __name__ == '__main__':
-    gauge = Gauge('python_vacuum_pressure_gauge', 'Vacuum chamber pressure')
-    start_http_server(8000)
+    # Gauge metric object
+    gauge = Gauge('vacuum_chamber_pressure', 'Vacuum chamber pressure')
+    # Expose the metrics at endpoint port 8001
+    start_http_server(8001)
     while True:
-        data = ser.readline()
-        print(data)
-        gauge.set(data)
+        process_request()
